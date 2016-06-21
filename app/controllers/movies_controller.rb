8232e081d7caaac3f0d8e7c2bf00b63f49b04853
Jake Lare <jlare@intelliquip.com>
2016-06-21 10:13:58 -0400

    Views and Controllers ERROR

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def index
-
+    @time = Time.now
+    @movies = ["Fiday Night Lights", "The Social Network", "Any Given Sunday", "Fight Club"]
   end
 end
